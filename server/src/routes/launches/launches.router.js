22e53c6508a48ee89af752615c98e39b8bd5f952
Oleh Lavryk <oleh.lavrik@gmail.com>
2023-04-06 18:38:10 +0300

    initial commit

---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpAbortLaunch
+} = require('./launches.controller')
+
+const launchesRouter = express.Router();
+
+launchesRouter
+  .get('/', httpGetAllLaunches)
+  .post('/', httpAddNewLaunch)
+  .delete('/:id', httpAbortLaunch);
+
+module.exports = launchesRouter;
