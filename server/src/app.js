422c8b15fe4197a2d6001acbccd5b0d9cd2cac67
Oleh Lavryk <olehlavryk@MacBook-Pro-Oleh.local>
2023-04-29 14:14:49 +0300

    feat: 🎸 add api versioning

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const path = require('path');
 const morgan = require('morgan');
-
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
+const api = require('./routes/api');
 
 const app = express();
 app.use(cors({
@@ -13,9 +11,8 @@ app.use(cors({
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/v1', api)
 
-app.use('/launches', launchesRouter);
-app.use('/planets', planetsRouter);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
---
+++ server/src/routes/api.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const planetsRouter = require('./planets/planets.router');
+const launchesRouter = require('./launches/launches.router');
+
+const api = express.Router();
+
+api.use('/launches', launchesRouter);
+api.use('/planets', planetsRouter);
+
+module.exports = api;
